9dd4c080175f2ba03648565820d7b6335d183c88
Mislav Srecec <mislav.srecec3@gmail.com>
2020-08-20 15:59:52 +0200

    Mongoose 1.3

--- models/dishes.js
+++ models/dishes.js
@@ -2,19 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const dishSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    description: {
-        type: String,
-        required: true
-    }
-},{
-    timestamps: true
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
 });
 
-var Dishes = mongoose.model('Dish', dishSchema);
+let Dishes = mongoose.model('Dish', dishSchema);
 
 module.exports = Dishes;
