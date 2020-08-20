0f73ab6cd487df0d48e8b842bed2c7acbf67474f
Mislav Srecec <mislav.srecec3@gmail.com>
2020-08-20 15:56:44 +0200

    Mongoose Part 1.2

--- index.js
+++ index.js
@@ -2,30 +2,34 @@ const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
 
-const url = 'mongodb://localhost27017/conFusion';
+const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
-  console.log('Connected correctly to server');
-  let newDish = Dishes({
-    name: 'Uthappizza',
-    description: 'test'
-  });
 
-  newDish.save()
-    .then((dish) => {
-      console.log(dish);
-      return Dishes.find({}).exec();
-    })
-    .then((dishes) => {
-      console.log(dishes);
+    console.log('Connected correctly to server');
 
-      return Dishes.remove({});
-    })
-    .then(() => {
-      return mongoose.connection.close();
-    })
-    .catch((err) => {
-      console.log(err);
-    })
+    var newDish = Dishes({
+        name: 'Uthappizza',
+        description: 'test'
+    });
+
+    newDish.save()
+        .then((dish) => {
+            console.log(dish);
+
+            return Dishes.find({});
+        })
+        .then((dishes) => {
+            console.log(dishes);
+
+            return Dishes.remove({});
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
 });
